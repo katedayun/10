775b881acfc7b76fe92a445b5fc0c03a663c70c9
katedayun <58518683+katedayun@users.noreply.github.com>
2023-11-09 15:43:44 +0900

    04

--- main2.c
+++ main2.c
@@ -5,12 +5,9 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
- 	char src[]="the worst thing";
- 	char dst[100];
- 	//empty space destination dst[100], put src ("")in the dst
- 	strcpy(dst,src);
- 	printf("copied string: %s",dst);
- 	
+ 	char str[30]="happy C programming";
+ 	printf("length of letter \" %s\" : %i", str, strlen(str));
+    
  	
 	
 	return 0;
