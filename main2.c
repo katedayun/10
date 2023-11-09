06526fcbda5ad380b99d3f9f9a3741d1d7cbcaa7
katedayun <58518683+katedayun@users.noreply.github.com>
2023-11-09 15:53:32 +0900

    05

--- main2.c
+++ main2.c
@@ -5,10 +5,23 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
- 	char str[30]="happy C programming";
- 	printf("length of letter \" %s\" : %i", str, strlen(str));
-    
- 	
+	FILE* fp;
+	int i;
+	char str[100];
+	//for repetition
+	for(i=0;i<3;i++){
+	
+	//1. open file
+		fp=fopen("sample.txt","w");
+	//2. write file
+	//2-1. print"input a word"
+		printf("input a word:");
+	//2-2. scanf a string
+		scanf("%s\n",str);
+	//2-3. fprintf()
+		fprintf(fp,"%s",str);
+	}
+	fclose(fp);	
 	
 	return 0;
 }
